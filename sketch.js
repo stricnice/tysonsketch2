0c64185e1aafe12dc3ee6968ce9e460266b37038
stricnice <marshtides@gmail.com>
2017-03-30 20:10:55 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var spot = {
+	x:100,
+	y:50
+}
+
+var col = {
+	r:255,
+	g:0,
+	b:0
+}
+
+
+function setup() {
+  createCanvas(800,800);
+	background(0);
+}
+
+function draw() {
+	noStroke();
+	spot.x=random(0,width);
+	spot.y=random(0,height);
+	col.r=random(0,40);
+	col.g=random(0,1);
+	col.b=random(0,255);
+  fill(col.r,col.g,col.b,175);
+	ellipse(spot.x,spot.y, 28,28);
+}
