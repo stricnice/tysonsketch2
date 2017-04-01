b53031167f2d4bfc749b13463bbaebcd5c138b2d
stricnice <marshtides@gmail.com>
2017-04-01 12:50:25 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,23 @@
-var spot = {
-	x:100,
-	y:50
-}
+//var x = random(0,width);
+//var y = random(0, height);
 
-var col = {
-	r:255,
-	g:0,
-	b:0
-}
 
 
 function setup() {
-  createCanvas(2000,2000);
-	background(0);
+	
+	createCanvas(400,400);
+	background(200);
+	
 }
 
-function draw() {
+function draw () {
+	x=random(0,width);
+	y=random(0,height);
 	noStroke();
-	spot.x=random(0,width);
-	spot.y=random(0,height);
-	col.r=random(0,40);
-	col.g=random(0,1);
-	col.b=random(0,255);
-  fill(col.r,col.g,col.b,175);
-	ellipse(spot.x,spot.y, 28,28);
+	fill(0,x,random(100,250),130);
+	ellipse(x,y,30,30);
+	
+	
+	
+	
 }
